55add74b2e00b06f20090adcde06c44eb12b8947
liuxiao <liuxiao2103@qq.com>
2022-03-10 01:51:42 +0800

    feat: 动态数组缩容

---
+++ First/03-链表/src/com/liuxiaocs/ArrayListReduce.java
@@ -0,0 +1,165 @@
+package com.liuxiaocs;
+
+/**
+ * 含有缩容操作的动态数组
+ *
+ * @param <E> 类型
+ */
+public class ArrayListReduce<E> extends AbstractList<E> {
+
+    /**
+     * 存储所有的元素
+     */
+    private E[] elements;
+
+    /**
+     * 默认初始容量
+     */
+    private static final int DEFAULT_CAPACITY = 10;
+
+    public ArrayListReduce(int capacity) {
+        capacity = (capacity < DEFAULT_CAPACITY) ? DEFAULT_CAPACITY : capacity;
+        elements = (E[]) new Object[capacity];
+    }
+
+    public ArrayListReduce() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    /**
+     * 清除所有元素的引用
+     */
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            elements[i] = null;
+        }
+        size = 0;
+    }
+
+    /**
+     * 获取index位置的元素
+     */
+    public E get(int index) {
+        rangeCheck(index);
+        return elements[index];
+    }
+
+    /**
+     * 设置index位置的元素
+     *
+     * @param index
+     * @param element
+     * @return 原来的元素
+     */
+    public E set(int index, E element) {
+        rangeCheck(index);
+        E old = elements[index];
+        elements[index] = element;
+        return old;
+    }
+
+    /**
+     * 在index位置插入一个元素
+     */
+    public void add(int index, E element) {
+        rangeCheckForAdd(index);
+        ensureCapacity(size + 1);
+
+        for (int i = size; i > index; i--) {
+            elements[i] = elements[i - 1];
+        }
+        elements[index] = element;
+        size++;
+    }
+
+    /**
+     * 删除index位置的元素
+     */
+    public E remove(int index) {
+        rangeCheck(index);
+        E old = elements[index];
+        for (int i = index + 1; i < size; i++) {
+            elements[i - 1] = elements[i];
+        }
+        elements[--size] = null;
+
+        trim();
+
+        return old;
+    }
+
+    /**
+     * 查看元素的索引
+     */
+    public int indexOf(E element) {
+        if (element == null) {
+            for (int i = 0; i < size; i++) {
+                if (elements[i] == null) return i;
+            }
+        } else {
+            for (int i = 0; i < size; i++) {
+                // 此时element一定不为空，放到前面
+                if (element.equals(elements[i])) return i;
+            }
+        }
+
+        return ELEMENT_NOT_FOUND;
+    }
+
+    /**
+     * 保证要有capacity的容量
+     *
+     * @param capacity
+     */
+    private void ensureCapacity(int capacity) {
+        int oldCapacity = elements.length;
+        if (oldCapacity >= capacity) return;
+
+        // newCapacity = oldCapacity * 1.5 新容量为旧容量的1.5倍
+        int newCapacity = oldCapacity + (oldCapacity >> 1);
+        E[] newElements = (E[]) new Object[newCapacity];
+        for (int i = 0; i < size; i++) {
+            newElements[i] = elements[i];
+        }
+        elements = newElements;
+
+        System.out.println(oldCapacity + "扩容为: " + newCapacity);
+    }
+
+    private void trim() {
+        int oldCapacity = elements.length;
+        // 剩余空间还很多
+        int newCapacity = oldCapacity >> 1;
+        // 容量太小也不缩容，避免频繁扩容
+        if (size >= newCapacity || oldCapacity <= DEFAULT_CAPACITY) return;
+
+        E[] newElements = (E[]) new Object[newCapacity];
+        for (int i = 0; i < size; i++) {
+            newElements[i] = elements[i];
+        }
+        elements = newElements;
+        System.out.println(oldCapacity + "缩容为: " + newCapacity);
+    }
+
+    /**
+     * 打印格式化
+     *
+     * @return 参考格式：size=3, [99, 88, 77]
+     */
+    @Override
+    public String toString() {
+        StringBuilder string = new StringBuilder();
+        string.append("size=").append(size).append(", [");
+        for (int i = 0; i < size; i++) {
+            if (i != 0) {
+                string.append(", ");
+            }
+            string.append(elements[i]);
+            // if(i != size - 1) {
+            //     string.append(", ");
+            // }
+        }
+        string.append("]");
+        return string.toString();
+    }
+}
--- First/03-链表/src/com/liuxiaocs/Main.java
+++ First/03-链表/src/com/liuxiaocs/Main.java
@@ -2,14 +2,51 @@ package com.liuxiaocs;
 
 public class Main {
     public static void main(String[] args) {
-        // List<Integer> list = new LinkedList<>();
-        List<Integer> list = new LinkedListWithDummy<>();
+        test1();
+        test2();
+        test3();
+    }
+
+    /**
+     * 测试单链表
+     */
+    private static void test1() {
+        List<Integer> list = new LinkedList<>();
         // list.remove(0);
         list.add(20);
         list.add(0, 10);
         list.add(30);
         list.add(list.size(), 40);
         list.remove(1);
+        System.out.println(list);
+    }
+
+    /**
+     * 测试带头节点单链表
+     */
+    private static void test2() {
+        List<Integer> list = new LinkedListWithDummy<>();
+        list.add(20);
+        list.add(0, 10);
+        list.add(30);
+        list.add(list.size(), 40);
+        list.remove(1);
+        System.out.println(list);
+    }
+
+    /**
+     * 测试动态数组缩容
+     */
+    private static void test3() {
+        List<Integer> list = new ArrayListReduce<>();
+        for (int i = 0; i < 50; i++) {
+            list.add(i);
+        }
+
+        for (int i = 0; i < 50; i++) {
+            list.remove(0);
+        }
+
         System.out.println(list);
     }
 }
