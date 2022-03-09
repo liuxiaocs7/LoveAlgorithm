7d2c9b3b85e02fdd6aa4e2f90fcfba1cea0021bb
liuxiao <liuxiao2103@qq.com>
2022-03-09 20:35:09 +0800

    feat: 更新单链表

---
+++ First/03-链表/src/com/liuxiaocs/LinkedList.java
@@ -0,0 +1,120 @@
+package com.liuxiaocs;
+
+public class LinkedList<E> extends AbstractList<E> {
+
+    /**
+     * 链表中中第一个节点
+     */
+    private Node<E> first;
+
+    /**
+     * 内部节点
+     *
+     * @param <E>
+     */
+    private static class Node<E> {
+        E element;
+        Node<E> next;
+
+        public Node(E element, Node<E> next) {
+            this.element = element;
+            this.next = next;
+        }
+    }
+
+    @Override
+    public void clear() {
+        size = 0;
+        first = null;
+    }
+
+    @Override
+    public E get(int index) {
+        return node(index).element;
+    }
+
+    @Override
+    public E set(int index, E element) {
+        Node<E> node = node(index);
+        E old = node.element;
+        node.element = element;
+        return old;
+    }
+
+    @Override
+    public void add(int index, E element) {
+        if (index == 0) {
+            first = new Node<>(element, first);
+        } else {
+            // 获取上一个节点
+            Node<E> prev = node(index - 1);
+            // 创建当前节点并更新指向
+            prev.next = new Node<>(element, prev.next);
+        }
+        size++;
+    }
+
+    @Override
+    public E remove(int index) {
+        Node<E> node = first;
+        if (index == 0) {
+            first = first.next;
+        } else {
+            Node<E> prev = node(index - 1);
+            node = prev.next;
+            prev.next = node.next;
+        }
+        size--;
+        return node.element;
+    }
+
+    @Override
+    public int indexOf(E element) {
+        Node<E> node = first;
+        if (element == null) {
+            for (int i = 0; i < size; i++) {
+                if (node.element == null) return i;
+                node = node.next;
+            }
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (element.equals(node.element)) return i;
+                node = node.next;
+            }
+        }
+
+        return ELEMENT_NOT_FOUND;
+    }
+
+    /**
+     * 获取指定下标位置的节点
+     *
+     * @param index 指定下标
+     * @return Node 对应节点
+     */
+    private Node<E> node(int index) {
+        rangeCheck(index);
+
+        Node<E> node = first;
+        for (int i = 0; i < index; i++) {
+            node = node.next;
+        }
+        return node;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder string = new StringBuilder();
+        string.append("size=").append(size).append(", [");
+        Node<E> node = first;
+        for (int i = 0; i < size; i++) {
+            if (i != 0) {
+                string.append(", ");
+            }
+            string.append(node.element);
+            node = node.next;
+        }
+        string.append("]");
+        return string.toString();
+    }
+}
